31c19259dc513031da0dda121f9f51ea863d7a0c
Jeff Jacobson-Swartfager <jeff.ja@gmail.com>
2017-11-29 16:35:39 -0800

    Generate Chinese contacts

--- user_generator.rb
+++ user_generator.rb
@@ -32,7 +32,8 @@ class UserGenerator
       people:      people_data,
       last_names:  last_name_data,
       locations:   location_data,
-      words:       CSV.read('/usr/share/dict/words').flatten,
+      #words:       CSV.read('/usr/share/dict/words').flatten,
+      words:       CSV.table('data/china/New_HSK_2010.csv'),
       base:        originals
     }
   end
@@ -62,15 +63,16 @@ class UserGenerator
   class << self
     def create_user(data)
       name = create_name(data[:people], data[:last_names])
-      location = create_location(data[:locations], data[:words])
+      location = create_location(data[:locations], data[:words][:word])
+      work_phone_type = rand(0..1) > 0.6 ? 'cell' : 'land'
       return {
         first_name: name[:first],
         last_name:  name[:last],
         icon:       create_icon(name),
-        phone_1:    create_phone_number,
-        phone_2:    create_phone_number,
-        phone_3:    create_phone_number,
-        email:      create_email(name, data[:words]),
+        phone_1:    create_phone_number(type: 'cell'),
+        phone_2:    create_phone_number(type: 'land'),
+        phone_3:    create_phone_number(type: work_phone_type),
+        email:      create_email(name, data[:words][:pinyin]),
         street:     location[:street],
         state:      location[:state],
         city:       location[:city],
@@ -93,29 +95,55 @@ class UserGenerator
       }
     end
 
-    private def create_phone_number
-      return "1-#{rand(100..999)}-#{'%003d' % rand(10**3)}-#{'%004d' % rand(10**4)}"
+    private def create_phone_number(type:)
+      case type
+      when 'land'
+        prefixes = []
+        prefixes.push(10,20,21,22,23,24,25,27,28,29,
+                      310,311,312,313,314,415,316,317,318,319,
+                      335,349,350,351,352,353,354,355,356,357,358,
+                      359,371,372,373,374,375,376,377,379,370,391,
+                      392,393,394,295,296,397,398)
+        prefix = prefixes.sample
+
+        return "(0#{prefix}) #{'%004d' % rand(10**4)} #{'%004d' % rand(10**4)}"
+      when 'cell'
+        prefixes = (130..139).to_a
+        prefixes.push(145,147,151,152,153,155,156,157,158,
+                      159,166,170,171,173,174,176,177,178,
+                      180,181,182,183,184,185,186,187,188,
+                      189,198,199)
+
+        return "#{prefixes.sample} #{'%004d' % rand(10**4)} #{'%004d' % rand(10**4)}"
+      end
     end
 
     private def create_email(name, word_list)
-      first = name[:first].downcase
-      last = name[:last].downcase
-      server = word_list.sample
-      domain = ["com","net","gov","org"].sample
+      first = word_list.sample.gsub(/\d*\s*/,"")
+      last = word_list.sample.gsub(/\d*\s*/,"")
+      server = word_list.sample.gsub(/\d*\s*/,"")
+      domain = ["ac","com","edu","gov","mil","net","org"].sample
 
-      return "#{first}.#{last}@#{server}.#{domain}"
+      return "#{first}.#{last}@#{server}.#{domain}.cn"
     end
 
     private def create_location(locations, word_list)
       location = locations.sample
-      road_type = [
-        "Rd", "Ln", "St", "Ave", "Ct", "Cir", "Bnd", "Blvd", 
-        "Dr", "Garden", "Hwy", "Jct", "Lk", "Loop", "Park", 
-        "Pkwy", "Pl", "Plz", "Rue", "Terrace", "Way"]
+      street_address = locations.sample[3]
+      house_number = rand(0..999)
+      room_number = rand(0..9999)
+
+      if rand(0..1) > 0.7 
+        street_address += "|#{house_number}"
+        if rand(0..1) > 0.7
+          street_address += "|#{room_number}"
+        end
+      end
+
       return {
-        state: location[2],
-        city:  location[0],
-        street: "#{rand(0..9999)} #{word_list.sample.capitalize} #{road_type.sample}"
+        state: location[0],
+        city:  "#{location[1]}|#{location[2]}",
+        street: street_address
       }
     end
 
